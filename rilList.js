bb38f9b09979274908580141608731d499c0342a
dnakamashi <diegonakamashi@gmail.com>
2011-03-14 20:55:06 -0300

    refactoring

---
+++ rilList.js
@@ -0,0 +1,21 @@
+function RilList(){
+
+}
+
+RilList.parse_2jsonOK = function(list_wrong){
+    var obj = JSON.parse(list_wrong);  
+    var list = obj.list;
+	var list_s =  JSON.stringify(list);	
+	list_s = list_s.replace("{", "[");
+	list_s = list_s.replace("}}", "}]");
+			
+	while(list_s.match(/\"[0-9]+\":/) != null)			
+	    list_s = list_s.replace(/\"[0-9]+\":/,"");					
+				
+    return list_s;	
+}
+
+RilList.parse_json2obj = function(list){
+    var list_obj = JSON.parse(list);			
+	return list_obj;
+}
