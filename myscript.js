a70a5a23333a6203de80f5d325bcb55f4adc4ae0
diegonakamashi <=>
2013-02-22 13:32:11 -0300

    refactoring

--- myscript.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('keyup', keyboardShortCut, false);//Keyboard ShortCut
-
-function keyboardShortCut(e){
-	if(e.altKey && e.ctrlKey){
-		chrome.extension.sendRequest({name : 'keyShortCut', keyCode : e.keyCode, url: window.location.href, title: document.title});
-	}
-}
